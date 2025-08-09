bc573d005e5bc5652ea237813c67ccf2026fcfdf
jimmy.ou <yanagiis@gmail.com>
2025-08-09 14:45:32 +0800

    feat: replace mini.diff with diffview

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
   { "mini.pairs", enabled = false },
+  { "echasnovski/mini.diff", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
 }
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -374,6 +374,75 @@ return {
     end,
   },
 
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen" },
+    opts = {
+      enhanced_diff_hl = true,
+      view = {
+        default = { winbar_info = true },
+        file_history = { winbar_info = true },
+      },
+      hooks = {
+        diff_buf_read = function(bufnr)
+          vim.b[bufnr].view_activated = false
+        end,
+      },
+    },
+  },
+
+  -- {
+  --   "yetone/avante.nvim",
+  --   event = "VeryLazy",
+  --   version = false, -- Never set this value to "*"! Never!
+  --   opts = {
+  --     provider = "ollama",
+  --     ollama = {
+  --       endpoint = 'http://192.168.18.208:11434',
+  --       model = "gemma3:12b",
+  --     },
+  --   },
+  --   build = "make",
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "stevearc/dressing.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     --- The below dependencies are optional,
+  --     "echasnovski/mini.pick", -- for file_selector provider mini.pick
+  --     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+  --     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+  --     "ibhagwan/fzf-lua", -- for file_selector provider fzf
+  --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  --     "zbirenbaum/copilot.lua", -- for providers='copilot'
+  --     {
+  --       -- support for image pasting
+  --       "HakonHarnes/img-clip.nvim",
+  --       event = "VeryLazy",
+  --       opts = {
+  --         -- recommended settings
+  --         default = {
+  --           embed_image_as_base64 = false,
+  --           prompt_for_file_name = false,
+  --           drag_and_drop = {
+  --             insert_mode = true,
+  --           },
+  --           -- required for Windows users
+  --           use_absolute_path = true,
+  --         },
+  --       },
+  --     },
+  --     {
+  --       -- Make sure to set this up properly if you have lazy=true
+  --       "MeanderingProgrammer/render-markdown.nvim",
+  --       opts = {
+  --         file_types = { "markdown", "Avante" },
+  --       },
+  --       ft = { "markdown", "Avante" },
+  --     },
+  --   },
+  -- },
+
   { import = "lazyvim.plugins.extras.ai.codeium" },
 
   { import = "lazyvim.plugins.extras.dap.core" },
